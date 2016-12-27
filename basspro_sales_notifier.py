b6c0b9418f2704e86726cf2fbe0b8e7b791d436d
isaacrael <grael@comcast.net>
2016-12-27 13:25:54 -0700

    "added some documentation also added the URL of the product to the body of the text message being sent if product is on sale"

--- basspro_sales_notifier.py
+++ basspro_sales_notifier.py
@@ -2,6 +2,10 @@
 # basspro_sales_notifier.py will notify Gil Rael via text message if item of interest
 # is found on the basspro.com web site
 
+# try and find ammo that is on sale to verify if your logic is correct for ammo sale items
+# may need to eliminate the try and except class since it seems that the element is found
+# but may not be displayed determine if class=sale can be found on the UMC 9mm page
+
 # import required modules
 
 from selenium import webdriver
@@ -21,9 +25,23 @@ def locate_sale_item():
     driver = webdriver.Firefox()
     driver.maximize_window()
 # open Caliberusa.com sales page
-    driver.get("http://www.basspro.com/Ascend-D10-SitIn-Kayak-Purple/product/11081905012222/")
+# next two commented out lines are used to test a product that is on sale vs a product that is not on sale
+#    driver.get("http://www.basspro.com/Ascend-D10-SitIn-Kayak-Purple/product/11081905012222/")
+#    driver.get("http://www.basspro.com/RedHead-FireResistant-10Gun-Safe/product/1602111742/
+    driver.get("http://www.basspro.com/Remington-UMC-Handgun-Ammo-Mega-Pack/product/53416/")
     driver.implicitly_wait(30)
-    element = driver.find_element_by_class_name("sale")
+    try:
+        element = driver.find_element_by_class_name("sale")
+        if element.is_displayed():
+            client = TwilioRestClient("AC2c8bb5d4f471c5ccf25765e4f757b030", "5183d8a42f3d1a9084a4325029132058")
+# change the "from_" number to your Twilio number and the "to" number
+# to the phone number you signed up for Twilio with, or upgrade your
+# account to send SMS to any phone number
+            client.messages.create(to="+15058330785", from_="+15057966457",
 
+                       body="Hello from Pi!.....ON SALE TODAY! : ) Time To Buy Basspro Remington UMC 9mm Ammo"
+                            "http://www.basspro.com/Remington-UMC-Handgun-Ammo-Mega-Pack/product/53416/")
+    except NoSuchElementException:
+        print("Item Not On Sale....")
     driver.quit()
 locate_sale_item();
