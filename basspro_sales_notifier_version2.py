4794f3185f867a27bf8aa006adeacbecefa8dcce
isaacrael <grael@comcast.net>
2016-12-28 09:55:55 -0700

    "version2 of this app trying to added logic if product is on sale and sku number matches product sku send text message also cleaned up code and added comments"

---
+++ basspro_sales_notifier_version2.py
@@ -0,0 +1,58 @@
+# Written By: Gil Rael
+# basspro_sales_notifier.py will notify Gil Rael via text message if item of interest
+# is found on the basspro.com web site
+
+# In version2 of this application trying to use logic so that if class="sale" is displayed found and
+# sku_number=product sku number # both conditions must be met for text messages to be sent
+# try and find ammo that is on sale to verify if your logic is correct for ammo sale items
+# may need to eliminate the try and except class since it seems that the element is found
+# but may not be displayed determine if class=sale can be found on the UMC 9mm page
+
+
+# import required modules
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+# this allows you to catch a thrown exception and add your own exception
+# so your script continues to run and exits normally
+from selenium.common.exceptions import NoSuchElementException
+from twilio.rest import TwilioRestClient
+
+
+# locate product of interest that is on sale at basspro.com and send
+# ON SALE TODAY text message if item is on sale
+# or send NO SALE TODAY text message if item of interest is not on sale
+
+def locate_sale_item():
+# open Firefox web browswer and maximize window
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+# open Caliberusa.com sales page
+# next three commented out lines are used to test a product that is on sale vs a product that is not on sale
+#    driver.get("http://www.basspro.com/Ascend-D10-SitIn-Kayak-Purple/product/11081905012222/")
+#    driver.get("http://www.basspro.com/RedHead-FireResistant-10Gun-Safe/product/1602111742/
+#    http://www.basspro.com/Remington-Premier-Golden-Saber-Centerfire-Handgun-Ammo/product/82328/
+#    driver.get("http://www.basspro.com/Remington-UMC-Handgun-Ammo-Mega-Pack/product/53416/")
+#    sale_url = "http://www.basspro.com/Remington-Premier-Golden-Saber-Centerfire-Handgun-Ammo/product/82328/"
+    driver.get("http://www.basspro.com/Remington-Premier-Golden-Saber-Centerfire-Handgun-Ammo/product/82328/")
+    driver.implicitly_wait(30)
+    page_title = driver.title
+    print(page_title)
+    try:
+#        element = driver.find_element_by_class_name("sale")
+        element_sku_number = driver.find_element_by_class_name("sku number")
+        sku_number = element_sku_number
+        print(sku_number)
+        if element.is_displayed():
+            client = TwilioRestClient("AC2c8bb5d4f471c5ccf25765e4f757b030", "5183d8a42f3d1a9084a4325029132058")
+# change the "from_" number to your Twilio number and the "to" number
+# to the phone number you signed up for Twilio with, or upgrade your
+# account to send SMS to any phone number
+            client.messages.create(to="+15058330785", from_="+15057966457",
+
+                       body="Hello from Pi!.....ON SALE TODAY! : ) Time To Buy Basspro Remington UMC 9mm Ammo"
+                           "http://www.basspro.com/Remington-UMC-Handgun-Ammo-Mega-Pack/product/53416/")
+    except NoSuchElementException:
+        print("Item Not On Sale....")
+    driver.quit()
+locate_sale_item();
